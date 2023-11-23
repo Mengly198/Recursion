ce75404b402492a1c74d0ce9ffbf3d2547421786
Mengly198 <2023194lay@aupp.edu.kh>
2023-11-23 08:44:37 +0700

    pushed

--- recursion.java
+++ recursion.java
@@ -1,10 +1,20 @@
 public class recursion {
   public static void main(String[] args) {
 
-    reverseString("Hello"); 
+    System.out.println(reverseString("Hello"));    
+    System.out.println(reverseString1("Hello"));
+
+
 
   }
 
+  public static String reverseString1(String str) {
+    StringBuilder reversed = new StringBuilder();
+    for (int i = str.length() - 1; i >= 0; i--) {
+        reversed.append(str.charAt(i));
+    }
+    return reversed.toString();
+  }
   public static String reverseString(String str) {
     if (str.isEmpty()) {
         return str;
